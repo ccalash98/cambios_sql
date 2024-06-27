19cc163854c3a7853f0a02dd76d55ec844c1f59b
ccalash98 <cristtofher.ccalash@gmail.com>
2024-06-27 14:28:34 -0500

    actualizacion menu if

--- ESTRUCTURAS/MENU_IF.sql
+++ ESTRUCTURAS/MENU_IF.sql
@@ -1,28 +1,34 @@
---//SCRIPT ID DE LA MENU RD
 
-
+--/////VALIDA SI EXISTE LOS MENUS O NO PARA INSERTAR LOS MENUS QUE FALTEN
 BEGIN;
+    DO $$
+    DECLARE
+        new_start_value INT;
+    BEGIN
+        -- Ejecutar la subconsulta para obtener el nuevo valor de inicio
+        SELECT count(*) + 1 INTO new_start_value FROM comercial.menu_rd;
+        
+        -- Usar ese valor en el comando RESTART WITH
+        EXECUTE 'ALTER TABLE comercial.menu_rd ALTER COLUMN menu_id RESTART WITH ' || new_start_value;
+    END $$;
 
--- Paso 1: Eliminar la clave primaria temporalmente
-ALTER TABLE comercial.menu_rd DROP CONSTRAINT pk_menu_rd;
+    BEGIN;
+        -- Paso 1: Eliminar la clave primaria temporalmente
+        ALTER TABLE comercial.menu_rd DROP CONSTRAINT pk_menu_rd;
 
--- Paso 2: Crear una columna temporal con nuevos IDs consecutivos
-ALTER TABLE comercial.menu_rd ADD COLUMN new_id SERIAL;
+        -- Paso 2: Crear una columna temporal con nuevos IDs consecutivos
+        ALTER TABLE comercial.menu_rd ADD COLUMN new_id SERIAL;
 
--- Paso 3: Actualizar la columna original de IDs con los valores de la columna temporal
-UPDATE comercial.menu_rd SET menu_id = new_id;
+        -- Paso 3: Actualizar la columna original de IDs con los valores de la columna temporal
+        UPDATE comercial.menu_rd SET menu_id = new_id;
 
--- Paso 4: Eliminar la columna temporal
-ALTER TABLE comercial.menu_rd DROP COLUMN new_id;
+        -- Paso 4: Eliminar la columna temporal
+        ALTER TABLE comercial.menu_rd DROP COLUMN new_id;
 
--- Paso 5: Restaurar la clave primaria
-ALTER TABLE comercial.menu_rd ADD CONSTRAINT pk_menu_rd PRIMARY KEY (menu_id);
+        -- Paso 5: Restaurar la clave primaria
+        ALTER TABLE comercial.menu_rd ADD CONSTRAINT pk_menu_rd PRIMARY KEY (menu_id);
 
-COMMIT;
-
-
-
---/////VALIDA SI EXISTE LOS MENUS O NO PARA INSERTAR LOS MENUS QUE FALTEN 
+    COMMIT;
 
     INSERT INTO comercial.menu_rd (menu_codigo, menu_nombre, menu_link, menu_target, menu_imagen, menu_ayuda_titulo, menu_ayuda_texto, menu_activo, menu_perfil, menu_orden, menu_tipo, menu_conti, menu_tip_rd, menu_cont_adm) 
     SELECT column1, column2, column3, column4, column5, column6, column7, column8, column9, column10, column11, column12, column13, column14
@@ -49,3 +55,4 @@ COMMIT;
         FROM comercial.menu_rd 
         WHERE CONCAT(menu_nombre, menu_link) = CONCAT(data.column2, data.column3)
     );
+COMMIT;
