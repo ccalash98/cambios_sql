6b531f0ddc2869d9b68ac4bfa30a748c84e5b131
ccalash98 <cristtofher.ccalash@gmail.com>
2024-07-24 15:42:03 -0500

    actualizacion consultas

--- ESTRUCTURAS/CODEX.sql
+++ ESTRUCTURAS/CODEX.sql
@@ -218,26 +218,15 @@ INSERT INTO comercial.doc_time (id_time, documento, time, estado, centro_costos,
 INSERT INTO comercial.menu_rd (menu_id, menu_codigo, menu_nombre, menu_link, menu_target, menu_imagen, menu_ayuda_titulo, menu_ayuda_texto, menu_activo, menu_perfil, menu_orden, menu_tipo, menu_conti, menu_tip_rd, menu_cont_adm) VALUES (1288, '2203', 'Zonas', 'config_zonas/zonas.php', 'main', 'fa fa-folder', 'Zonas', 'Zonas', 'S', '110011000000000000000000000000', 1, 'M', 0, 'L', 'A');
 UPDATE comercial.menu_rd SET menu_nombre = 'Regiones' WHERE menu_id = 122;
 
---///// validacion de inserts if exists
+--///// consulta para traer dos columnas
 
-
-INSERT INTO comercial.menu_rd (menu_id, menu_codigo, menu_nombre, menu_link, menu_target, menu_imagen, menu_ayuda_titulo, menu_ayuda_texto, menu_activo, menu_perfil, menu_orden, menu_tipo, menu_conti, menu_tip_rd, menu_cont_adm) 
-SELECT * FROM (
-    VALUES 
-    (9990, '010806', 'Ejecucion Incidencia', 'asignar_incidencia/registro.php', 'main', 'ico_nuevo.png', 'Registro Ticket', 'Registro Ticket', 'S', '111111000000000110010100000000', 1, 'M', 0, 'L', 'C'),
-    (9991, '010807', 'Productividad Incidencias', 'int_rep_incidencia/registro.php', 'main', 'ico_nuevo.png', 'Productividad Incidencias', 'Productividad Incidencias', 'S', '111010000001000010000000000000', 1, 'M', 0, 'L', 'C'),
-    (9992, '010808', 'Conceptos-Clasificaciones', 'int_ccpt_mtv/registro.php', 'main', 'ico_nuevo.png', 'Conceptos-Clasificaciones', 'Conceptos-Clasificaciones', 'S', '110000000001000010000000000000', 1, 'M', 0, 'L', 'C'),
-    (501003, '-00106', 'Productividad', 'dashboard_produ/dashboard.php', 'main', 'fa fa-tachometer', 'Productividad', 'Productividad', 'S', '110011000010010000000000000000', 1, 'M', 0, 'L', 'C'),
-    (1239, '0100', 'Gestion WS', 'app_menu.php', 'menu', 'ico_nuevo.png', 'Gestión WS', 'Gestión WS', 'S', '110000000000000010000100000000', 2, 'D', 0, 'O', 'C'),
-    (1240, '010001', 'Olt', 'int_ws_olt/olt.php', 'main', 'ico_nuevo.png', 'OLT', 'OLT', 'S', '111011000100010010000100000000', 1, 'M', 0, 'L', 'C'),
-    (1242, '010003', 'Vinculo Olt - Router', 'int_ws_olt_router/olt_router.php', 'main', 'ico_nuevo.png', 'Vinculo Olt - Router', 'Vinculo Olt - Router', 'S', '111011000100010010000100000000', 1, 'M', 0, 'L', 'C'),
-    (1241, '010002', 'Router', 'int_ws_router/router.php', 'main', 'ico_nuevo.png', 'Router', 'Router', 'S', '111011000100010010000100000000', 1, 'M', 0, 'L', 'C'),
-    (1243, '010004', 'Onus', 'int_activar_equipo/activacion.php', 'main', 'ico_nuevo.png', 'Onus', 'Onus', 'S', '111011000100010010000100000000', 1, 'M', 0, 'L', 'C'),
-    (1244, '010004', 'Tareas programadas', 'int_ws_tareas/tareas.php', 'main', 'ico_nuevo.png', 'Tareas programadas', 'Tareas programadas', 'S', '111011000100010010000100000000', 1, 'M', 0, 'L', 'C')
-) AS data
-WHERE NOT EXISTS (
-    SELECT 1 FROM comercial.menu_rd WHERE menu_id = data.column1
-);
+SELECT 
+	empr_nom_empr , sucu_nom_sucu
+FROM saeempr
+inner join saesucu
+on
+	empr_cod_empr = sucu_cod_empr
+ORDER BY sucu_cod_sucu
 
 
 --////// cambiar las nc y fac cuando estan mal los valores
